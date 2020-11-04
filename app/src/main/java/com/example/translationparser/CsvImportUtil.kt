2da4db82062cf24d22bf886786d95b41e67f26b6
Arne Solty <arne.solty@dataport.de>
2020-11-04 12:54:14 +0100

    c

--- app/src/main/java/com/example/translationparser/CsvImportUtil.kt
+++ app/src/main/java/com/example/translationparser/CsvImportUtil.kt
@@ -7,43 +7,34 @@ import java.io.InputStream
 
 class CsvImportUtil(private var context: Context, private var fileName: String) {
 
-    companion object {
+    companion object{
         private const val columnTitle = 0
         private const val columnContent = 1
 
-        private fun getRows(inputStream: InputStream): List<List<String>> {
-            return csvReader {
+        private fun getRows(inputStream: InputStream): List<List<String>>{
+            return csvReader{
                 delimiter = ';'
             }.readAll(inputStream)
         }
     }
 
-    fun createFiles() {
+    fun createFiles(){
         createXML2()
     }
 
-    private fun createXML() {
+    private fun createXML(){
         val rows = getRows(context.resources.assets.open(fileName))
-        for (row in rows) {
+        for( row in rows){
             System.out.println(row)
         }
     }
 
-    private fun createXML2() {
-        val csvReader = CSVReaderBuilder(FileReader(fileName))
-                .withCSVParser(CSVParserBuilder().withSeparator(';').build())
-                .build()
-
-        // Maybe do something with the header if there is one
-        val header = csvReader.readNext()
-
-        // Read the rest
-        var line: Array<String>? = csvReader.readNext()
-        while (line != null) {
-            // Do something with the data
-            println(line[0])
-
-            line = csvReader.readNext()
+    private fun createXML2(){
+        csvReader().open(fileName) {
+            readAllAsSequence().forEach { row ->
+                //Do something with the data
+                println(row)
+            }
         }
     }
 }
